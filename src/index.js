557a0804882eb5841ffb11a481e2dfef7bca46d3
kuz-en <bio19@ya.ru>
2021-09-12 17:58:56 +0300

    get data from ipify

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const ipInput = document.querySelector('.search-bar__input');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', getData);
+ipInput.addEventListener('keydown', handleKey);
+
+function getData() {
+  fetch(`
+  https://geo.ipify.org/api/v1?apiKey=at_uUXrCcPXIHM8aaAQf7Q6YtAiS2ivQ&ipAddress=${ipInput.value}`)
+    .then(response => response.json())
+    .then(console.log);
+}
+
+function handleKey(e) {
+  if (e.key === 'Enter') {
+    getData();
+  }
+}
