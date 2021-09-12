6f912686b38e7bf63110ca5de298ed204014ab0b
kuz-en <bio19@ya.ru>
2021-09-12 18:38:58 +0300

    output ip data in html

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import {validateIp} from './helpers';
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
 
+const ipInfo = document.querySelector('#ip');
+const locationInfo = document.querySelector('#location');
+const timezoneInfo = document.querySelector('#timezone');
+const ispInfo = document.querySelector('#isp');
+
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
@@ -11,7 +16,7 @@ function getData() {
     fetch(`
     https://geo.ipify.org/api/v1?apiKey=at_uUXrCcPXIHM8aaAQf7Q6YtAiS2ivQ&ipAddress=${ipInput.value}`)
       .then(response => response.json())
-      .then(console.log);
+      .then(setInfo);
   }
 }
 
@@ -19,4 +24,11 @@ function handleKey(e) {
   if (e.key === 'Enter') {
     getData();
   }
+}
+
+function setInfo(mapData) {
+  ipInfo.innerText = mapData.ip;
+  locationInfo.innerText = mapData.location.country + ' ' + mapData.location.region;
+  timezoneInfo.innerText = mapData.location.timezone;
+  ispInfo.innerText = mapData.isp;
 }
